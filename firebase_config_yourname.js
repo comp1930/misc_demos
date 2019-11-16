7114f7e573f6d44f292c4f63c62ee6467efd2834
Carly Orr <carly_wong_orr@bcit.ca>
2019-11-15 21:46:33 -0800

    first commit

---
+++ firebase_config_yourname.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+
+       // your api keys etc.
+       // copy a chunk of code from firebase console and paste here
+
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Create a reference to the firestore database
+const db = firebase.firestore();
